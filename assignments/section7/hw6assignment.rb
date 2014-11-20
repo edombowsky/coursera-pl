3381b2b9aab714dddf531d14e1e22d62718256d3
edombowsky <earl.dombowsky@ventyx.abb.com>
2014-11-20 13:53:20 -0800

    complete assignment

--- assignments/section7/hw6assignment.rb
+++ assignments/section7/hw6assignment.rb
@@ -5,18 +5,87 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = [rotations([[0, 0], [-1, 0], [-1, -1], [0, -1], [1, 0]]),
+                             [[[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+                              [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]],
+                   rotations([[0, 0], [0, -1], [1, -1]])] + All_Pieces
+  Cheat_Piece = [[[0, 0]]]
 
   # your enhancements here
+  def self.next_piece (board)
+    if board.cheat
+      MyPiece.new(Cheat_Piece, board)
+    else
+      MyPiece.new(All_My_Pieces.sample, board)
+    end
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
+  attr_reader :cheat
+
+  def initialize (game)
+    super
+    @cheat         = false
+    @cheat_cost    = 100
+    @current_block = MyPiece.next_piece(self)
+  end
+
+  def rotate_180
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+
+    draw
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @cheat         = false
+    @current_pos   = nil
+  end
+
+  def store_current
+    locations    = @current_block.current_rotation
+    displacement = @current_block.position
+
+    (0..(locations.size - 1)).each{|index|
+        current = locations[index]
+        @grid[current[1] + displacement[1]][current[0]+displacement[0]] = @current_pos[index]
+    }
+
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
+  def cheat_next
+    if @score > @cheat_cost and !@cheat
+      @score -= @cheat_cost
+      @cheat = true
+    end
+  end
 
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board  = MyBoard.new(self)
+
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+
+    @board.draw
+  end
+
+  def key_bindings
+    super
+    @root.bind('u', proc {@board.rotate_180})
+    @root.bind('c', proc {@board.cheat_next})
+  end
 
 end
 
